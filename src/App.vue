f9cf1ea1e788d4d6445f12a7ff86630bf26f2517
Vagia <vagiakaraba@gmail.com>
2025-02-15 11:33:46 +0200

    Create initial vue app with vite

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+      </nav>
+    </div>
+  </header>
+  <RouterView />
+</template>
