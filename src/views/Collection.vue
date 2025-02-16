ea5b6b0edeb8b119dc47b468ef40163d9fb2e566
Vagia <vagiakaraba@gmail.com>
2025-02-16 11:23:53 +0200

    Show saved jokes in collection route

--- src/views/Collection.vue
+++ src/views/Collection.vue
@@ -1,1 +1,21 @@
-<template>Collection page</template>
+<script setup lang="ts">
+import { useCollectionStore } from '@/stores/useCollectionStore'
+
+const { jokeCollection } = useCollectionStore()
+</script>
+
+<template>
+  <div v-if="jokeCollection.length > 0">
+    <h2>Collection</h2>
+    <ul>
+      <li v-for="joke in jokeCollection" :key="joke.id">
+        <p><strong>Joke Type:</strong> {{ joke.type }}</p>
+        <h3>{{ joke.setup }}</h3>
+        <p>{{ joke.punchline }}</p>
+      </li>
+    </ul>
+  </div>
+  <div v-else>
+    <p>No jokes in collection</p>
+  </div>
+</template>
